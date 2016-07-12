3ddb57c2a824c852308a252aea90532ca4e76a4b
vensder@gmail.com <vensder@gmail.com>
2016-07-12 17:22:40 -0400

    ptr.c

--- ptr.c
+++ ptr.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-
+    int size;
     int a = 10;
 
     int *ptr = NULL;
-
+    size = sizeof(ptr);
+    printf("%d\n", size);
     ptr = &a;
 //    printf("%d",10);
 //    printf("%d",a);
