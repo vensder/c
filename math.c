2ad04b2aff0d8b050da33f12816b0ed1be152ecb
vensder <vensder@gmail.com>
2016-07-28 12:06:26 +0300

    math.c

--- math.c
+++ math.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    printf("%d\n", 0+50*1-60-60*0+10);
+
+    printf("%f\n", 3/4);
+    printf("%f\n", 7/2);
+    printf("%d\n", 0+50*1-60-60*0+10.0);
+    printf("%d\n", 160+17.0);
+    printf("%d\n", 16.0+17);
+    printf("%d\n", 16+17.0);
+
+    printf("%f\n", 3/4);
+    printf("%f\n", 7/2);
+
     return 0;
 }
