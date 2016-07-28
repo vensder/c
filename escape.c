35144380fa77f0c458fdba788794f3d2ca3901e7
vensder <vensder@gmail.com>
2016-07-28 06:35:41 +0300

    escape.c

---
+++ escape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("12345\b\b");
+    printf("54321\b\b");
+
+    return 0;
+
+}
---
+++ math.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("%d\n", 0+50*1-60-60*0+10);
+    return 0;
+}
