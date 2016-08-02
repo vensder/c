1850a3f3d884063b0c26811ec0c405f0ef3c4167
vensder <vensder@gmail.com>
2016-08-03 00:53:45 +0300

    test.c

--- test.c
+++ test.c
@@ -2,11 +2,19 @@
 
 int main()
 {
-    int c;
+    int cc;
 
     printf("%f\n", 6/55.0);
-    c = getchar();
-    printf("%d\n", c);
-    putchar (c);
+    cc = getchar();
+    printf("%d\n", cc);
+    putchar (cc);
     printf("\n");
+
+    char a,b,c,d;
+    a = 'W';
+    b = a +24;
+    c = b + 8;
+    d = '\n';
+    printf("%c%c%c%c", a,b,c,d);
+
 }
