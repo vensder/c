c6a4ca616a651b1097d8c2ff15c96ac1907419fc
vensder <vensder@gmail.com>
2016-08-03 00:02:04 +0300

    pointers.c

--- pointers.c
+++ pointers.c
@@ -3,10 +3,8 @@
 int main(){
 
     const int DECADE = 10;
-
     int *p;
     int x;
-
     p = &DECADE;
     *p = 11;
     printf("DECADE = %d \n", DECADE);
@@ -19,6 +17,27 @@ int main(){
 
     printf("p = %p \n", p);
     printf("*p = %d \n", *p);
+
+    int a = 7, b= 17;
+    int *pa, *pb;
+
+    pa = &a; pb = &b;
+    printf("a = %d, b = %d \n", a, b);
+    printf("*pa = %d, *pb = %d \n", *pa, *pb);
+    pa = &b; pb = &a;
+    printf("*pa = %d, *pb = %d \n", *pa, *pb);
+
+    int array[2] = {7,17};
+    int *parray[2];
+
+    parray[0] = &array[0]; parray[1] = &array[1];
+    printf("%d, %d \n", *parray[0], *parray[1]);    
+    parray[0]++; parray[1]--;
+    printf("%d, %d \n", *parray[0], *parray[1]);
+
+
+
+    printf("linux = %d \n", linux);
 
 }
 
