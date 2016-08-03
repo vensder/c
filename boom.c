78c8947e6c2e3ce33f5de74dbef02685c7c2e45f
vensder <vensder@gmail.com>
2016-08-03 22:41:38 +0300

    boom.c

--- boom.c
+++ boom.c
@@ -13,18 +13,17 @@ int main(){
     }
 
     char hello[] = "Press Enter for the explosion!";
-    printf("%s \n", hello);
+    printf("%s", hello);
 
     getchar();
 
-    printf("Countdown: \n");
     for (int i = 0; i < 100; i++){
         usleep(30000);
         printf("->\b");
         fflush(stdout);
     }
 
-    printf("\n Finished!\n");
+    printf("\nFinished!\n");
 
 
     return 0;
