c9631cdcb2e56b7fd99557cb25cbe44144b3a787
Dmitry Makarov <vensder@gmail.com>
2016-08-13 23:45:59 +0300

    system.c

--- system.c
+++ system.c
@@ -1,5 +1,6 @@
 #include <stdlib.h> //system
 #include <stdio.h> //printf
+#include <string.h> //strcmp
 
 int main(){
 	int ret_val;
@@ -8,17 +9,20 @@ int main(){
 	FILE *fp = popen("uname", "r");
 	fscanf(fp, "%s", uname);
 	pclose(fp);
-	printf("uname: %s\n", uname);
-	exit(0);
 
-	if (!system("which zenity > /dev/null")){
+	printf("uname: %s\n", uname);
+
+	if (!strcmp(uname,"Darwin")){
+		if (!system("which osascript > /dev/null")){
+			ret_val = system("osascript -e 'display dialog \"It is TIME\"'");
+			printf("%d\n", ret_val);
+		}
+	}
+
+	else if (!system("which zenity > /dev/null")){
 		ret_val = system("zenity --info --text=Hello");
 		printf("%d\n", ret_val);
 	}
 
-	if (!system("which osascript > /dev/null")){
-		ret_val = system("osascript -e 'display dialog \"It is TIME\"'");
-		printf("%d\n", ret_val);
-	}
-
+	else printf("zenity is not installed on your system\n");
 }
