da8559588c5ffb7f815e52c5e68960c2db8324de
Dmitry Makarov <vensder@gmail.com>
2016-08-14 23:14:31 +0300

    arg.c

--- arg.c
+++ arg.c
@@ -3,11 +3,14 @@
 int main(int argc, char *argv[])
 {
     if (argc > 1){
-        printf("Quantity of args is %d\n", argc-1);
-        for (int i = 1; i < argc; i++){
-            printf("%d: %s\n", i, argv[i]);
-        }
-    }
+        printf("Quantity of args is %d:\n", argc-1);
 
-    return(0);
+	for (int i = 1; i < argc; i++){
+            printf("%d: %s; ", i, argv[i]);
+        }
+	printf("\n");
+
+	for (char **p=argv; *p!=NULL; p++)
+		printf("%s\n", p[0]);
+    }
 }
