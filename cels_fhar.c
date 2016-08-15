ffea607e3d358d984c50a6a0a1a28977b50f4606
Dmitry Makarov <vensder@gmail.com>
2016-08-15 23:39:26 +0300

    cels_fhar.c

--- cels_fhar.c
+++ cels_fhar.c
@@ -15,15 +15,23 @@ float cels_to_fahr(int c){
 	return (9.0/5.0) * c + 32.0;
 }
 
+void print_line(){
+	printf("------------||------------\n");
+}
+
+void print_head(){
+	printf("Cels  Fahr  ||  Fahr  Cels\n");
+}
+
 int main()
 {
-	printf("Fahr \t Cels\n");
+	print_line();
+	print_head();
+	print_line();
 	for (int t = LOWER; t <= UPPER; t+=STEP){
-		printf("%3d \t %3.0f \n", t, fahr_to_cels(t));
+		printf("%4d  %4.0f  ||  ", t, cels_to_fahr(t));
+		printf("%4d  %4.0f\n", t, fahr_to_cels(t));
 	}
 
-	printf("Cels \t Fahr\n");
-	for (int t = LOWER; t <= UPPER; t+=STEP){
-		printf("%3d \t %3.0f \n", t, cels_to_fahr(t));
-	}
+	print_line();
 }    
